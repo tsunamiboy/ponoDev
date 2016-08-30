99c252e143f2e6a5a3fa92c63595bd0107602c3a
David Blake <dvdblake63@gmail.com>
2016-08-30 13:43:19 -0700

    Initial Commit

---
+++ Pono v. 0.01/ViewController.swift
@@ -0,0 +1,85 @@
+//
+//  ViewController.swift
+//  Pono v. 0.01
+//
+//  Created by David Blake on 8/17/16.
+//  Copyright © 2016 Sea Turtle Dreams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    let clock = Clock()
+    
+    @IBOutlet weak var clockViewLabel: UILabel!
+    
+    @IBOutlet weak var timerViewLabel: UILabel!
+
+    
+//    var time = NSDate()
+    
+    var seconds = 0
+    var timer = NSTimer()
+    var timerIsOn = false
+   
+    
+    func updateTimer() {
+        seconds += 1
+        timerViewLabel.text = "\(seconds)"
+    }
+    
+    func updateClock() {
+        let formatter = NSDateFormatter()
+        formatter.timeStyle = .MediumStyle
+        clockViewLabel.text = "\(formatter.stringFromDate(clock.currentTime))"
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+       
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: #selector(ViewController.updateClock), name: UIApplicationWillEnterForegroundNotification, object: nil)
+            }
+    
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        updateClock()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    @IBAction func startButton(sender: AnyObject) {
+        
+        if timerIsOn == false {
+            timer = NSTimer.scheduledTimerWithTimeInterval(1, target: self, selector: (#selector(ViewController.updateTimer)), userInfo: nil, repeats: true)
+            timerIsOn = true
+        }
+    }
+    
+    @IBAction func stopButton(sender: AnyObject) {
+        timer.invalidate()
+        timerIsOn = false
+    }
+    
+    @IBAction func refreshButton(sender: AnyObject) {
+//        timer.invalidate()
+        seconds = 0
+        timerViewLabel.text = "\(seconds)"
+//        timerIsOn = false
+    }
+    
+
+
+}
+
---
+++ Pono v. 0.01/clock.swift
@@ -0,0 +1,17 @@
+//
+//  clock.swift
+//  Pono v. 0.01
+//
+//  Created by David Blake on 8/29/16.
+//  Copyright © 2016 Sea Turtle Dreams. All rights reserved.
+//
+
+import Foundation
+
+class Clock {
+    
+    var currentTime: NSDate {
+        return NSDate()
+    }
+    
+}
